ae24cdc4f075ced6a05d567e0f11b85e83755847
xiaohua <wxhdax@gmail.com>
2017-09-14 23:17:53 -0700

    Fifth

--- app/src/main/java/edu/neu/madcourse/xiaohuawang/Aty1.java
+++ app/src/main/java/edu/neu/madcourse/xiaohuawang/Aty1.java
@@ -1,10 +1,12 @@
 package edu.neu.madcourse.xiaohuawang;
 
+import android.Manifest;
 import android.content.Context;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.support.annotation.Nullable;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
@@ -18,25 +20,45 @@ import edu.neu.madcourse.xiaohuawang.R;
 
 public class Aty1 extends AppCompatActivity {
 
+    private static final int PERMISSIONS_REQUEST_READ_PHONE_STATE = 999;
     private Button buttonClose;
     private TextView uniqueID;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("XiaohuaWang");//set the title bar
         setContentView(R.layout.aty1);
 
         buttonClose = (Button) findViewById(R.id.buttonClose);
         buttonClose.setOnClickListener(new View.OnClickListener() {
-            @Override
+
             public void onClick(View view) {
                 finish();
             }
         });
 
-        ((TextView) findViewById(R.id.uniqueId)).setText(generateId());
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE},
+                    PERMISSIONS_REQUEST_READ_PHONE_STATE);
+        } else {
+            String deviceId = generateId();
+            ((TextView) findViewById(R.id.uniqueId)).setText(deviceId);
+        }
+//        ((TextView) findViewById(R.id.uniqueId)).setText(generateId());
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_PHONE_STATE
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            generateId();
+            String deviceId = generateId();
+            ((TextView) findViewById(R.id.uniqueId)).setText(deviceId);
+        }
     }
 
     public String generateId() {
@@ -44,8 +66,11 @@ public class Aty1 extends AppCompatActivity {
 //        String deviceId = android.provider.Settings.Secure.getString(this.getContentResolver(),
 //                Settings.Secure.ANDROID_ID);
 
-        TelephonyManager tm=(TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        String deviceId=tm.getDeviceId();
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String deviceId = tm.getDeviceId();
+        if (deviceId == null) {
+            deviceId = "no id exist";
+        }
         return deviceId;
 
     }
